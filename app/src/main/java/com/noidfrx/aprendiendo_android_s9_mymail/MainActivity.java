56c5e9dd4679290efe25c7a5a757fb83385ab619
noidfrx <edufco9@gmail.com>
2021-02-04 17:18:22 -0300

    Initial commit

---
+++ app/src/main/java/com/noidfrx/aprendiendo_android_s9_mymail/MainActivity.java
@@ -0,0 +1,14 @@
+package com.noidfrx.aprendiendo_android_s9_mymail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
